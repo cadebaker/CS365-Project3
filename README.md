911fd6489f6f0ee492e30f94a8a33165d3c0374f
Cade Baker <cadebaker1@gmail.com>
2019-04-03 03:52:00 -0400

    Created GUI for classifying images

---
+++ README.md
@@ -0,0 +1,23 @@
+# Overview
+
+This repo contains code for the "TensorFlow for poets 2" series of Google Codelabs.
+
+### Compile Updated .UI File
+pyuic5 Project3UI.ui -o Project3UI.py
+
+
+### Run Application GUI
+python runGUI.py
+
+### Retrain Code
+python -m scripts.retrain \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --model_dir=tf_files/models/"${ARCHITECTURE}" \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph=tf_files/retrained_graph.pb \
+  --output_labels=tf_files/retrained_labels.txt \
+  --image_dir=tf_files/PHOTO_DIR
+
+### Classify Image
+python -m scripts.label_image \
+    --image=tf_files/IMG_URL
---
+++ runGUI.py
@@ -0,0 +1,51 @@
+import sys
+import subprocess
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+from PyQt5.QtGui import QIcon, QPixmap
+from Project3UI import Ui_MainWindow
+sys.path.insert(0, './scripts')
+from label_image_gui import classifyMain
+
+#qtCreatorFile = "Project3UI.ui" # Enter file here.
+
+#Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    file = ''
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.chooseFile.clicked.connect(self.selectFile)
+        self.imageLabel.mousePressEvent = self.selectFile
+        self.classifyButton.clicked.connect(self.runClassify)
+        self.actionExit.triggered.connect(QtCore.QCoreApplication.instance().quit)
+
+    def selectFile(self, val2):
+        self.resultsDataLabel.setText("")
+        global file
+        file = QFileDialog.getOpenFileName()[0]
+        self.imageLabel.setText(file)
+        pixmap = QPixmap(file).scaledToWidth(461);
+        self.imageLabel.setPixmap(pixmap)
+
+    def convertToPercent(self, val):
+        return "{:.3%}".format(val)
+
+    def runClassify(self):
+        if(file != ''):
+            returnedClassifyData = classifyMain(file)
+            print("------ Results ------")
+            template = "{} {:0.5f}"
+            #print(returnedClassifyData[0][0])
+            #print(returnedClassifyData[0][1])
+            for i in range(len(returnedClassifyData)):
+                print(template.format(returnedClassifyData[i][0], returnedClassifyData[i][1]))
+            self.resultsDataLabel.setText("The top result was " + returnedClassifyData[0][0] + " with a probability of " + self.convertToPercent(returnedClassifyData[0][1]))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
